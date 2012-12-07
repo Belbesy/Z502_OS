9163d3d5d36be90451b367c0e21d542bba9675fb
Belbesy <mohmdadel@gmail.com>
2012-12-07 15:13:21 +0200

    round robing.h

--- src/os/sched/policies/roundrobin.h
+++ src/os/sched/policies/roundrobin.h
@@ -5,11 +5,8 @@ class roundrobin_scheduler{
 	start;
 
 	void k(){
-		 __asm
-		   {
-		      mov eax, num    ; Get first argument
-		      mov ecx, power  ; Get second argument
-		      shl eax, cl     ; EAX = EAX * ( 2 to the power of CL )
-		   }
+
+
+
 	}
 };
